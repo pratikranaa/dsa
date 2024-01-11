06321de36a4ab5f41525b238f554e48f726caafc
Pratik Rana <pratikrana859@gmail.com>
2024-01-11 16:32:15 +0530

    Lecture 01

---
+++ hello_world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    for (int i =0; i <10; i++){
+        cout << "Hello World! a " << endl;
+        for (int j =0; j <10; j++){
+            cout << " Hello World! b " ;
+        }
+       cout << endl;
+    }
+    return 0;
+}
